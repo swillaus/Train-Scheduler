f7f5fe2c461eebe837159e095a06a35bb8bdd40c
Your Name <sam@upsidebusinessgroup.com.au>
2019-08-25 22:16:51 -0400

    addition of basic logic

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,44 +1,45 @@
 // Input Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyA1cfdFkSz-wUPV9DOer0tixPAXNkw9LLM",
-    authDomain: "train-schedule-ca8d1.firebaseapp.com",
-    databaseURL: "https://train-schedule-ca8d1.firebaseio.com",
-    projectId: "train-schedule-ca8d1",
-    storageBucket: "",
-    messagingSenderId: "65586820247",
-    appId: "1:65586820247:web:9b604dbee7f4f4a9"
-  };
+   apiKey: "AIzaSyA1cfdFkSz-wUPV9DOer0tixPAXNkw9LLM",
+   authDomain: "train-schedule-ca8d1.firebaseapp.com",
+   databaseURL: "https://train-schedule-ca8d1.firebaseio.com",
+   projectId: "train-schedule-ca8d1",
+   storageBucket: "",
+   messagingSenderId: "65586820247",
+   appId: "1:65586820247:web:9b604dbee7f4f4a9"
+};
 
- firebase.initializeApp(firebaseConfig);
- var database = firebase.database();
- var name = "";
- var role = "";
- var startDate = 0;
- var monthlyRate = 0;
- $("#submitValue").on("click", function (event) {
-    event.preventDefault();
-    name = $("#employeeName").val().trim();
-    role = $("#roleInput").val().trim();
-    startDate = $("#startDate").val().trim();
-    monthlyRate = $("#monthlyRate").val().trim();
-    // push to firebase
-    database.ref().push({
-        name: name,
-        role: role,
-        startDate: startDate,
-        monthlyRate: monthlyRate,
-    });
- });
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var trainName = "";
+var role = "";
+var startDate = 0;
+var monthlyRate = 0;
+
+$("#submitValue").on("click", function (event) {
+   event.preventDefault();
+   trainName = $("#trainName").val().trim();
+   destination = $("#destination").val().trim();
+   firstTrainTime = $("#firstTrainTime").val().trim();
+   frequency = $("#frequency").val().trim();
+   // push to firebase
+   database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrainTime: firstTrainTime,
+      frequency: frequency,
+   });
+});
 
 
- database.ref().on("child_added", function (snapshot) {
-    var sv = snapshot.val();
-    var name = childSnapshot.val().name;
-    var role =  childSnapshot.val().role;
-    var startDate =  childSnapshot.val().startDate;
-    var monthlyRate = childSnapshot.val().monthlyRate;
-    console.log(sv.name);
-    console.log(sv.role);
-    console.log(sv.startDate);
-    console.log(sv.monthlyRate);
- })
+database.ref().on("child_added", function (snapshot) {
+   var sv = snapshot.val();
+   var name = childSnapshot.val().trainName;
+   var role = childSnapshot.val().destination;
+   var startDate = childSnapshot.val().firstTrainTime;
+   var monthlyRate = childSnapshot.val().frequency;
+   console.log(sv.name);
+   console.log(sv.role);
+   console.log(sv.startDate);
+   console.log(sv.monthlyRate);
+})
